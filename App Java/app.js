7c4144c66b139e3aad49b65468e5a8f7a34f95cf
UP210582 <up210582@alumnos.upa.edu.mx>
2024-02-20 01:14:28 -0600

    Final Count

--- App Java/app.js
+++ App Java/app.js
@@ -1,2 +1,21 @@
-let btn = document.getElementById("btn");
-console.log(btn);
+let clicks = 0;
+const countClicks = document.querySelector('#count');
+const [increment, decrement, reset] = document.querySelectorAll('button');
+
+document.addEventListener('DOMContentLoaded', () => {
+  increment.onclick = () => {
+    clicks++;
+    countClicks.innerHTML = clicks;
+  };
+
+  decrement.onclick = () => {
+    clicks--;
+    countClicks.innerHTML = clicks;
+  };
+
+  reset.onclick = () => {
+    clicks = 0;
+    countClicks.innerHTML = 0;
+  };
+  
+});
